caced56ee8e784cb180bf1e8de9892ab424a4768
Ben Halverson <benhalverson33@gmail.com>
2015-07-03 20:29:37 -0700

    added more defaults

--- app.js
+++ app.js
@@ -46,10 +46,19 @@ angular.module('scheduler', ['firebase'])
       tuesday: {
         name: 'Tuesday',
         slots: {
+          700: {
+            time: '7:00am',
+            booked: false
+          },
+          800: {
+            time: '8:00am',
+            booked: false
+          },
           900: {
             time: '9:00am',
             booked: false
           },
+
           0110: {
             time: '11:00am',
             booked: false
@@ -98,9 +107,80 @@ angular.module('scheduler', ['firebase'])
           700: {
             time: '7:00pm',
             booked: false
+          },
+          800: {
+            time: '8:00pm',
+            booked: false
           }
-    	  }
+        }
+      },
+      thursday: {
+        name: 'Thursday',
+        slots: {
+          900: {
+            time: '9:00am',
+            booked: false
+          },
+          0110: {
+            time: '11:00am',
+            booked: false
+          },
+          100: {
+            time: '1:00pm',
+            booked: false
+          },
+          300: {
+            time: '3:00pm',
+            booked: false
+          },
+          500: {
+            time: '5:00pm',
+            booked: false
+          },
+          700: {
+            time: '7:00pm',
+            booked: false
+          },
+          800: {
+            time: '8:00pm',
+            booked: false
+          }
+        }
+      },
+      friday: {
+        name: 'Friday',
+        slots: {
+          900: {
+            time: '9:00am',
+            booked: false
+          },
+          0110: {
+            time: '11:00am',
+            booked: false
+          },
+          100: {
+            time: '1:00pm',
+            booked: false
+          },
+          300: {
+            time: '3:00pm',
+            booked: false
+          },
+          500: {
+            time: '5:00pm',
+            booked: false
+          },
+          700: {
+            time: '7:00pm',
+            booked: false
+          },
+          800: {
+            time: '8:00pm',
+            booked: false
+          }
+        }
       }
+
     });
 
   };
