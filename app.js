bdb884915e5f8240452c44997b729f73028e7102
Ben Halverson <benhalverson33@gmail.com>
2015-07-03 20:06:25 -0700

    sample angularfire app

--- app.js
+++ app.js
@@ -5,37 +5,106 @@ angular.module('scheduler', ['firebase'])
   var ref = new Firebase('https://myschedule-app.firebaseio.com/days');
   var fb = $firebase(ref);
 
-  defaults
+  //sync as object
+  var syncObject = fb.$asObject();
+
+  // three way data binding
+  syncObject.$bindTo($scope, 'days');
+
   $scope.reset = function() {
-    console.log('clicked');
+
     fb.$set({
-   monday: {
-     name: 'Monday',
-     slots: {
-       0900: {
-         time: '9:00am',
-         booked: false
-       },
-       0110: {
-         time: '11:00am',
-         booked: false
-       }
-     }
-   },
-   tuesday: {
-     name: 'Tuesday',
-     slots: {
-       0900: {
-         time: '9:00am',
-         booked: false
-       },
-       0110: {
-         time: '11:00am',
-         booked: false
-       }
-     }
-   }
- });
+      monday: {
+        name: 'Monday',
+        slots: {
+          900: {
+            time: '9:00am',
+            booked: false
+          },
+          0110: {
+            time: '11:00am',
+            booked: false
+          },
+          100: {
+            time: '1:00pm',
+            booked: false
+          },
+          300: {
+            time: '3:00pm',
+            booked: false
+          },
+          500: {
+            time: '5:00pm',
+            booked: false
+          },
+          700: {
+            time: '7:00pm',
+            booked: false
+          }
+    	  }
+      },
+      tuesday: {
+        name: 'Tuesday',
+        slots: {
+          900: {
+            time: '9:00am',
+            booked: false
+          },
+          0110: {
+            time: '11:00am',
+            booked: false
+          },
+          100: {
+            time: '1:00pm',
+            booked: false
+          },
+          300: {
+            time: '3:00pm',
+            booked: false
+          },
+          500: {
+            time: '5:00pm',
+            booked: false
+          },
+          700: {
+            time: '7:00pm',
+            booked: false
+          }
+    	  }
+      },
+      wednesday: {
+        name: 'Wednesday',
+        slots: {
+          900: {
+            time: '9:00am',
+            booked: false
+          },
+          0110: {
+            time: '11:00am',
+            booked: false
+          },
+          100: {
+            time: '1:00pm',
+            booked: false
+          },
+          300: {
+            time: '3:00pm',
+            booked: false
+          },
+          500: {
+            time: '5:00pm',
+            booked: false
+          },
+          700: {
+            time: '7:00pm',
+            booked: false
+          }
+    	  }
+      }
+    });
+
   };
-var syncObject = fb.$asOjbect
+
+
+
 });
